b236437dbfee372e4f2f3ddf8c41964fed71ca14
Pedro Castro <devpedrocastro@gmail.com>
2023-05-01 22:45:27 -0300

    Aula06

--- Aula05/index.js
+++ Aula05/index.js
@@ -1,2 +1,10 @@
 //Variáveis e Tipos Primitivos - Curso JavaScript #05
 
+//identificadoores ----------------------------------
+//podem comecar com letar, $ ou _
+//nao podem comecar com numeros
+//é possivel usar letras ou numeros 
+//é possivel usar acentos e simbolos 
+//nao podem conter espacos
+//nao podem ser palavras reservadas
+
